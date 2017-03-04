0c1eb4ca7f5f676586271942c948ea1a9365bac0
jonknoll <jonknoll@hotmail.com>
2017-03-04 01:15:27 -0800

    possible working snake

--- app/main.py
+++ app/main.py
@@ -1,27 +1,68 @@
 import bottle
 import os
 import random
+import grid
 
 
 # Values to put in grid, priorities TBD
 OPEN_SPACE = 0
-ME_SNAKE = 1
-WALL = 2
-FOOD = 3
-OTHER_SNAKE = 4
+ME_SNAKE = 's'
+WALL = -1
+FOOD = 'F'
+OTHER_SNAKE = 'S'
 BIG_SNAKE = 5
-
+DEAD_SNAKE = 'D'
+SNAKE_HEAD = 'H'
 
 
 
 def build_grid(data):
-    pass
+    height = data['height']
+    width = data['width']
+    myId = data['you']
+    
+    grid = grid.Grid(width, height)
+    
+    # fill with the living snakes
+    for snake in data['snakes']:
+        id = snake['id']
+        if(id == myId):
+            snakeType = ME_SNAKE
+        else:
+            snakeType = OTHER_SNAKE
+        
+        grid.setList(snake['coords'], snakeType)
+        if(snakeType != ME_SNAKE):
+            grid.set([snake['coords'][0][0], snake['coords'][0][1]], SNAKE_HEAD)
+        
+    # fill with the dead snakes
+    for snake in data['dead_snakes']:
+        grid.setList(snake['coords'], DEAD_SNAKE)
 
+    # fill with food
+    for food in data['food']:
+        grid.setList(data['food'], FOOD)
 
+    return(grid)
 
+def getOurHeadCoord(data):
+    myId = data['you']
+    for snake in data['snakes']:
+        id = snake['id']
+        if(id == myId):
+            return(snake['coords'][0])
 
-
-
+def coordToDirection(currentCoord, proposedCoord):
+    if((proposedCoord[0] - currentCoord[0]) == 1):
+        direction = 'right'
+    elif((proposedCoord[0] - currentCoord[0]) == -1):
+        direction = 'left'
+    elif((proposedCoord[1] - currentCoord[1]) == 1):
+        direction = 'up'
+    else:
+        direction = 'down'
+    return(direction)
+    
 
 
 @bottle.route('/static/<path:path>')
@@ -61,6 +102,8 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+    grid = build_grid(data)
+    ourCoord = getOurHeadCoord()
     
     print("!SNAKE MOVE!")
     for k,v in data.iteritems():
@@ -70,10 +113,22 @@ def move():
         print("SNAKE!")
         for k,v in snake.iteritems():
             print("{}={}".format(k,v))
+    print("\n\n")
+    grid.printGrid()
+    print("\nWe are at {}".format(ourCoord))
 
     # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
-
+    #directions = ['up', 'down', 'left', 'right']
+    directions = []
+    if(grid.get([ourCoord[0]-1,ourCoord[1]]) == 0):
+        directions.append('left')
+    if(grid.get([ourCoord[0]+1,ourCoord[1]]) == 0):
+        directions.append('right')
+    if(grid.get([ourCoord[0],ourCoord[1]+1]) == 0):
+        directions.append('up')
+    if(grid.get([ourCoord[0],ourCoord[1]-1]) == 0):
+        directions.append('down')
+    
     return {
         'move': random.choice(directions),
         'taunt': 'battlesnake-python!'
